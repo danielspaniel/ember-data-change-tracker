ceb00b2927fbb370d4327b5d7e43a4bb6a155ece
Dan Sudol <dansudol@yahoo.com>
2017-11-16 15:36:23 +0000

    fixing transform lookup that fixes a few bugs

--- addon/tracker.js
+++ addon/tracker.js
@@ -85,7 +85,7 @@ export default class Tracker {
    */
   static transformFn(model, attributeType) {
     let transformType = attributeType || 'object';
-    return model.store.serializerFor(model.constructor.modelName).transformFor(transformType);
+    return this.container(model).lookup(`transform:${transformType}`);  
   }
 
   /**
--- tests/unit/tracker-test.js
+++ tests/unit/tracker-test.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
 import Tracker from 'ember-data-change-tracker/tracker';
-import {make, makeList, manualSetup}  from 'ember-data-factory-guy';
-import {initializer as modelInitializer} from 'ember-data-change-tracker';
-import {test, moduleFor} from 'ember-qunit';
+import FactoryGuy, { make, makeList, manualSetup } from 'ember-data-factory-guy';
+import { initializer as modelInitializer } from 'ember-data-change-tracker';
+import { test, moduleFor } from 'ember-qunit';
 import sinon from 'sinon';
-const { A } = Ember;
-const { w } = Ember.String;
+
+const {A} = Ember;
+const {w} = Ember.String;
 modelInitializer();
 
 moduleFor('model:company', 'Unit | Tracker', {
@@ -21,19 +22,19 @@ test('#envConfig returns the config for the application environment', function(a
   assert.deepEqual(Tracker.envConfig(company), {});
 });
 
-test('#options with valid options', sinon.test(function(assert) {
+test('#options with valid options', function(assert) {
   let company = make('company');
-  let envConfig = this.stub(Tracker, 'envConfig');
-  let modelConfig = this.stub(Tracker, 'modelConfig');
+  let envConfig = sinon.stub(Tracker, 'envConfig');
+  let modelConfig = sinon.stub(Tracker, 'modelConfig');
 
   let tests = [
-    [{}, {}, { trackHasMany: true, auto: false, enableIsDirty: false }],
-    [{ trackHasMany: false }, {}, { trackHasMany: false, auto: false, enableIsDirty: false }],
-    [{ trackHasMany: true }, { only: ['info'] }, { auto: false, trackHasMany: true, only: ['info'], enableIsDirty: false }],
-    [{}, { only: ['info'] }, { only: ['info'], auto: false, trackHasMany: true, enableIsDirty: false }],
-    [{}, { except: ['info'] }, { except: ['info'], auto: false, trackHasMany: true, enableIsDirty: false }],
-    [{ enableIsDirty: true }, { except: ['info'] }, { except: ['info'], auto: false, trackHasMany: true, enableIsDirty: true }],
-    [{}, { enableIsDirty: true }, { auto: false, trackHasMany: true, enableIsDirty: true }],
+    [{}, {}, {trackHasMany: true, auto: false, enableIsDirty: false}],
+    [{trackHasMany: false}, {}, {trackHasMany: false, auto: false, enableIsDirty: false}],
+    [{trackHasMany: true}, {only: ['info']}, {auto: false, trackHasMany: true, only: ['info'], enableIsDirty: false}],
+    [{}, {only: ['info']}, {only: ['info'], auto: false, trackHasMany: true, enableIsDirty: false}],
+    [{}, {except: ['info']}, {except: ['info'], auto: false, trackHasMany: true, enableIsDirty: false}],
+    [{enableIsDirty: true}, {except: ['info']}, {except: ['info'], auto: false, trackHasMany: true, enableIsDirty: true}],
+    [{}, {enableIsDirty: true}, {auto: false, trackHasMany: true, enableIsDirty: true}],
   ];
 
   for (let test of tests) {
@@ -42,30 +43,33 @@ test('#options with valid options', sinon.test(function(assert) {
     modelConfig.returns(modelOpts);
     assert.deepEqual(Tracker.options(company), expectedOptions);
   }
-}));
+
+  Tracker.envConfig.restore();
+  Tracker.modelConfig.restore();
+});
 
 test('#options with invalid options', function(assert) {
   let company = make('company');
 
-  company.set('changeTracker', { dude: "where's my car" });
-  assert.throws(() => Tracker.options(company), `[ember-data-change-tracker] 
-    changeTracker options can have 'only', 'except' , 'auto', or 
+  company.set('changeTracker', {dude: "where's my car"});
+  assert.throws(() => Tracker.options(company), `[ember-data-change-tracker]
+    changeTracker options can have 'only', 'except' , 'auto', or
     'trackHasMany' but you are declaring: dude`);
 });
 
-test('#getTrackerKeys', sinon.test(function(assert) {
+test('#getTrackerKeys', function(assert) {
   let user = make('user');
-  let envConfig = this.stub(Tracker, 'envConfig');
-  let modelConfig = this.stub(Tracker, 'modelConfig');
+  let envConfig = sinon.stub(Tracker, 'envConfig');
+  let modelConfig = sinon.stub(Tracker, 'modelConfig');
 
   let tests = [
     [{}, {}, 'info blob company profile projects pets'],
-    [{ trackHasMany: false }, {}, 'info blob company profile'],
-    [{ trackHasMany: true }, { only: ['info'] }, 'info'],
-    [{}, { only: ['info'] }, 'info'],
-    [{}, { except: ['info'] }, 'blob company profile projects pets'],
-    [{ auto: true }, {}, 'info blob company profile projects pets', true],
-    [{ enableIsDirty: true }, {}, 'info blob company profile projects pets', false, true],
+    [{trackHasMany: false}, {}, 'info blob company profile'],
+    [{trackHasMany: true}, {only: ['info']}, 'info'],
+    [{}, {only: ['info']}, 'info'],
+    [{}, {except: ['info']}, 'blob company profile projects pets'],
+    [{auto: true}, {}, 'info blob company profile projects pets', true],
+    [{enableIsDirty: true}, {}, 'info blob company profile projects pets', false, true],
   ];
 
   for (let test of tests) {
@@ -83,11 +87,14 @@ test('#getTrackerKeys', sinon.test(function(assert) {
     assert.equal(info.autoSave, expectedAutoSave, 'auto save setting');
     assert.equal(info.enableIsDirty, expectedEnableIsDirty, 'enableIsDirty setting');
   }
-}));
 
-test('#serialize', sinon.test(function(assert) {
-  let envConfig = this.stub(Tracker, 'envConfig');
-  envConfig.returns({ trackHasMany: true, auto: true });
+  Tracker.envConfig.restore();
+  Tracker.modelConfig.restore();
+});
+
+test('#serialize', function(assert) {
+  let envConfig = sinon.stub(Tracker, 'envConfig');
+  envConfig.returns({trackHasMany: true, auto: true});
 
   let company = make('company');
   let profile = make('profile');
@@ -96,23 +103,34 @@ test('#serialize', sinon.test(function(assert) {
 
   let tests = [
     ['info', null, "null"],
-    ['info', { dude: 1 }, '{"dude":1}'],
+    ['info', {dude: 1}, '{"dude":1}'],
     ['profile', null, null],
     ['profile', profile, profile.id],
     ['company', null, null],
-    ['company', company, { id: company.id, type: company.constructor.modelName }],
-    ['projects', undefined, null],
+    ['company', company, {id: company.id, type: company.constructor.modelName}],
+    //    ['projects', undefined, null],
     ['projects', projects, A(projects).mapBy('id')],
-    ['pets', pets, pets.map((p) => {
-      return { id: p.id, type: p.constructor.modelName };
-    })],
+    ['pets', pets, pets.map((p) => ({id: p.id, type: p.constructor.modelName}))],
   ];
 
   for (let test of tests) {
     let [key, value, expectedSerialized] = test;
-    let user = make('user', { [key]: value });
+    let user = make('user', {[key]: value});
 
     let serializedValue = Tracker.serialize(user, key);
     assert.deepEqual(serializedValue, expectedSerialized);
   }
-}));
+  Tracker.envConfig.restore();
+});
+
+moduleFor('model:company', 'Unit | previously unloaded model test', {
+  integration: true,
+  beforeEach: function() {
+    manualSetup(this.container);
+  }
+});
+
+test('transformFn when creating new record', function(assert) {
+  Ember.run(() => FactoryGuy.store.createRecord('user'));
+  assert.ok(true);
+});
