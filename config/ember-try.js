54b933e6f8dd595442277ccf481023e426289b4e
Chris Thoburn <cthoburn@linkedin.com>
2018-08-15 16:10:45 -0700

    remove bower from ember-try

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,111 +1,87 @@
 /* eslint-env node */
-module.exports = {
-  scenarios: [
-    {
-      name: 'ember-data-2.7',
-      bower: {
-        devDependencies: {},
-        resolutions: {}
-      },
-      npm: {
-        devDependencies: {
-          'ember-source': '2.11',
-          'ember-data': '2.7.0',
-          'ember-data-model-fragments': '2.11',
-          'ember-inflector': '1.9.4'
-        }
-      }
-    },
-    {
-      name: 'ember-data-2.8',
-      bower: {
-        devDependencies: {},
-        resolutions: {}
-      },
-      npm: {
-        devDependencies: {
-          'ember-source': '2.11',
-          'ember-data': 'v2.8.1',
-          'ember-data-model-fragments': '2.11',
-          'ember-inflector': '1.9.4'
-        }
-      }
-    },
-    {
-      name: 'ember-data-2.10',
-      bower: {
-        devDependencies: {},
-        resolutions: {}
-      },
-      npm: {
-        devDependencies: {
-          'ember-source': '2.11',
-          'ember-data': '2.10.0',
-          'ember-data-model-fragments': '2.11'
-        }
-      }
-    },
-    {
-      name: 'ember-data-3.0',
-      bower: {
-        devDependencies: {},
-        resolutions: {}
-      },
-      npm: {
-        devDependencies: {
-          'ember-source': '3.0',
-          'ember-data': '3.0',
-          'ember-data-model-fragments': '3.0.0-beta.1'
-        }
-      }
-    },
-    {
-      name: 'ember-release',
-      bower: {
-        devDependencies: {
-          'ember': 'components/ember#release'
+'use strict';
+
+const getChannelURL = require('ember-source-channel-url');
+
+module.exports = function() {
+  return Promise.all([
+    getChannelURL('release'),
+    getChannelURL('beta'),
+    getChannelURL('canary'),
+  ]).then(urls => {
+    return {
+      useYarn: true,
+      scenarios: [
+        {
+          name: 'ember-data-2.7',
+          npm: {
+            devDependencies: {
+              'ember-source': '2.11',
+              'ember-data': '2.7.0',
+              'ember-data-model-fragments': '2.11',
+              'ember-inflector': '1.9.4'
+            }
+          }
         },
-        resolutions: {
-          'ember': 'release'
-        }
-      },
-      npm: {
-        devDependencies: {
-          'ember-data': 'emberjs/data#release'
-        }
-      }
-    },
-    {
-      name: 'ember-beta',
-      bower: {
-        devDependencies: {
-          'ember': 'components/ember#beta'
+        {
+          name: 'ember-data-2.8',
+          npm: {
+            devDependencies: {
+              'ember-source': '2.11',
+              'ember-data': 'v2.8.1',
+              'ember-data-model-fragments': '2.11',
+              'ember-inflector': '1.9.4'
+            }
+          }
         },
-        resolutions: {
-          'ember': 'beta'
-        }
-      },
-      npm: {
-        devDependencies: {
-          'ember-data': 'emberjs/data#beta'
-        }
-      }
-    },
-    {
-      name: 'ember-canary',
-      bower: {
-        devDependencies: {
-          'ember': 'components/ember#canary'
+        {
+          name: 'ember-data-2.10',
+          npm: {
+            devDependencies: {
+              'ember-source': '2.11',
+              'ember-data': '2.10.0',
+              'ember-data-model-fragments': '2.11'
+            }
+          }
         },
-        resolutions: {
-          'ember': 'canary'
+        {
+          name: 'ember-data-3.0',
+          npm: {
+            devDependencies: {
+              'ember-source': '3.0',
+              'ember-data': '3.0',
+              'ember-data-model-fragments': '3.0.0-beta.1'
+            }
+          }
+        },
+        {
+          name: 'ember-release',
+          npm: {
+            devDependencies: {
+              'ember-data': 'emberjs/data#release',
+              'ember-source': urls[0],
+            }
+          }
+        },
+        {
+          name: 'ember-beta',
+          npm: {
+            devDependencies: {
+              'ember-data': 'emberjs/data#beta',
+              'ember-source': urls[1],
+            }
+          }
+        },
+        {
+          name: 'ember-canary',
+          npm: {
+            devDependencies: {
+              'ember-data': 'emberjs/data#canary',
+              'ember-source': urls[2],
+            }
+          }
         }
-      },
-      npm: {
-        devDependencies: {
-          'ember-data': 'emberjs/data#canary'
-        }
-      }
-    }
-  ]
+      ]
+    };
+  });
 };
