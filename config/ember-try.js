8c9ad036fdc4c733a61d77514ce28d54cb1d8510
Dan Sudol <dansudol@yahoo.com>
2019-08-06 08:19:43 -0400

    fixing CI

--- config/ember-try.js
+++ config/ember-try.js
@@ -13,52 +13,10 @@ module.exports = function() {
       useYarn: true,
       scenarios: [
         {
-          name: 'ember-data-2.7',
-          npm: {
-            devDependencies: {
-              'ember-source': '2.11',
-              'ember-data': '2.7.0',
-              'ember-data-model-fragments': '2.11',
-              'ember-inflector': '1.9.4'
-            }
-          }
-        },
-        {
-          name: 'ember-data-2.8',
-          npm: {
-            devDependencies: {
-              'ember-source': '2.11',
-              'ember-data': 'v2.8.1',
-              'ember-data-model-fragments': '2.11',
-              'ember-inflector': '1.9.4'
-            }
-          }
-        },
-        {
-          name: 'ember-data-2.10',
-          npm: {
-            devDependencies: {
-              'ember-source': '2.11',
-              'ember-data': '2.10.0',
-              'ember-data-model-fragments': '2.11'
-            }
-          }
-        },
-        {
-          name: 'ember-data-3.0',
-          npm: {
-            devDependencies: {
-              'ember-source': '3.0',
-              'ember-data': '3.0',
-              'ember-data-model-fragments': '3.0.0-beta.1'
-            }
-          }
-        },
-        {
           name: 'ember-release',
           npm: {
             devDependencies: {
-              'ember-data': 'emberjs/data#release',
+              'ember-data': 'release',
               'ember-source': urls[0],
             }
           }
@@ -67,7 +25,7 @@ module.exports = function() {
           name: 'ember-beta',
           npm: {
             devDependencies: {
-              'ember-data': 'emberjs/data#beta',
+              'ember-data': 'beta',
               'ember-source': urls[1],
             }
           }
@@ -76,7 +34,7 @@ module.exports = function() {
           name: 'ember-canary',
           npm: {
             devDependencies: {
-              'ember-data': 'emberjs/data#canary',
+              'ember-data': 'canary',
               'ember-source': urls[2],
             }
           }
